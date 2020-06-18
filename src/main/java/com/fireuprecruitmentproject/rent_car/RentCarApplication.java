fa215e85ff0661b73cffb8aed3248fe28c3fc51c
michal turocha <michalturocha@gmail.com>
2020-06-18 14:48:50 +0200

    first commit

--- src/main/java/com/fireuprecruitmentproject/rent_car/RentCarApplication.java
+++ src/main/java/com/fireuprecruitmentproject/rent_car/RentCarApplication.java
@@ -3,8 +3,10 @@ package com.fireuprecruitmentproject.rent_car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:secretPassword")
 public class RentCarApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/fireuprecruitmentproject/rent_car/controller/CarController.java
+++ src/main/java/com/fireuprecruitmentproject/rent_car/controller/CarController.java
@@ -3,7 +3,12 @@ package com.fireuprecruitmentproject.rent_car.controller;
 import com.fireuprecruitmentproject.rent_car.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/cars")
@@ -13,6 +18,13 @@ public class CarController {
     @Autowired
     public CarController(CarRepository carRepository) {
         this.carRepository = carRepository;
+    }
+
+    @GetMapping("/all")
+    public ModelAndView showAllCars(){
+        Map<String,Object> cars = new HashMap<>();
+        cars.put("samochody",carRepository.findAll());
+        return new ModelAndView("samochody",cars);
     }
 
 
